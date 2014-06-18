b093639cf11e964c0982c5a192e501be1e618d5b
Damiaan Twelker <damiaan@appnormal.com>
2014-06-18 16:03:23 +0200

    master: built custom activity for launch

---
+++ master/app/src/main/java/nl/uva/ncc/SlavesActivity.java
@@ -0,0 +1,17 @@
+package nl.uva.ncc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import se.bitcraze.crazyfliecontrol.R;
+
+public class SlavesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slaves);
+    }
+    
+}
