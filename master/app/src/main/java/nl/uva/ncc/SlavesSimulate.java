099a7364c736db26a5bca4e9488d0ea1724cb387
Kristian Korrel <kristian.korrel@student.uva.nl>
2014-06-25 13:24:43 +0200

    working on correct scaling in visualization

--- master/app/src/main/java/nl/uva/ncc/SlavesSimulate.java
+++ master/app/src/main/java/nl/uva/ncc/SlavesSimulate.java
@@ -6,7 +6,10 @@ package nl.uva.ncc;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -24,6 +27,8 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import se.bitcraze.crazyfliecontrol.R;
+
+import static java.lang.Math.abs;
 
 public class SlavesSimulate extends Activity {
 
@@ -55,29 +60,97 @@ public class SlavesSimulate extends Activity {
 //        });
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        updateSizeInfo();
+    }
+
+    public void updateSizeInfo() {
+
+    }
+
     private void visualize_devices() {
         Intent intent = getIntent();
         // get the names and coordinates of connected devices
         ArrayList<String> mNames = intent.getStringArrayListExtra("slavesnames");
         ArrayList<String> mSlavesLat = intent.getStringArrayListExtra("slaveslat");
         ArrayList<String> mSlavesLon = intent.getStringArrayListExtra("slaveslon");
+        int nSlaves = mNames.size();
 
         // add new textview with info to layout for every device
         RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relative_layout);
         RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
 
-        Random r = new Random();
-        for(int i = 0; i < mNames.size(); i++) {
-            TextView deviceTextview = new TextView(this);
-            deviceTextview.setText(mNames.get(i) + " " + mSlavesLat.get(i) + " " + mSlavesLon.get(i));
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int canvasWidth = size.x;
+        int canvasHeight = size.y;
 
-            //TODO.. set margin relative to coordinates
-            lp.leftMargin = 50 + r.nextInt(500);
-            lp.topMargin = 50 + r.nextInt(500);
+        if (nSlaves == 0) {
+            return;
+        }
+
+        else if (nSlaves == 1) {
+            TextView deviceTextview = new TextView(this);
+            deviceTextview.setText(mNames.get(0) + " " + mSlavesLat.get(0) + " " + mSlavesLon.get(0));
+            lp.leftMargin = canvasWidth / 2;
+            lp.topMargin = canvasHeight / 2;
+
+            Log.d("", "width: " + canvasWidth + " " + canvasWidth / 2);
 
             //add view to layout
             relativeLayout.addView(deviceTextview, lp);
         }
-    }
 
+        else {
+            // Get smallest and largest lats and longs to scale properly
+            float smallestLon = Float.parseFloat(mSlavesLon.get(0));
+            float largestLon = Float.parseFloat(mSlavesLon.get(0));
+            float smallestLat = Float.parseFloat(mSlavesLat.get(0));
+            float largestLat = Float.parseFloat(mSlavesLat.get(0));
+
+            for (int i = 1; i < nSlaves; i++) {
+                float lon = Float.parseFloat(mSlavesLon.get(i));
+                float lat = Float.parseFloat(mSlavesLat.get(i));
+
+                if (lon < smallestLon) {
+                    smallestLon = lon;
+                } else {
+                    largestLon = lon;
+                }
+
+                if (lat < smallestLat) {
+                    smallestLat = lat;
+                } else {
+                    largestLat = lat;
+                }
+            }
+
+            float totalLonDiff = abs(largestLon - smallestLon);
+            float totalLatDiff = abs(largestLat - smallestLat);
+
+            Random r = new Random();
+            for (int i = 0; i < nSlaves; i++) {
+                TextView deviceTextview = new TextView(this);
+                deviceTextview.setText(mNames.get(i) + " " + mSlavesLat.get(i) + " " + mSlavesLon.get(i));
+
+                float lon = Float.parseFloat(mSlavesLon.get(i));
+                float lat = Float.parseFloat(mSlavesLat.get(i));
+
+                float lonDiff = abs(lon - smallestLon);
+                float latDiff = abs(lat - smallestLat);
+
+                double xScale = lonDiff / totalLonDiff;
+                double yScale = latDiff / totalLatDiff;
+
+                lp.leftMargin = 50 + (int) (xScale * (canvasWidth - 100));
+                lp.topMargin = 50 + (int) (yScale * (canvasHeight - 100));
+
+                //add view to layout
+                relativeLayout.addView(deviceTextview, lp);
+            }
+        }
+    }
 }
